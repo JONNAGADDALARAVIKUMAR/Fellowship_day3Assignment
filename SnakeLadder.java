200150013e7de6beae55e1da3fdf38db3cef1c41
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-10-23 17:04:09 +0530

    UC 7

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -6,43 +6,79 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		
-			System.out.println("Snake and Ladder game by Single player");
+			System.out.println("\n\tSnake and Ladder game by Two players\n\n");
 			
 			
 			Random random = new Random();
-			int position = 0;		
+			int firstPlayerPosition = 0, secondPlayerPosition=0;		
 			int option;
-			int diceValue,count=0;
+			int diceValue,firstPlayerChances=0,secondPlayerChances=0;
 			
-
-			
-			while(position < 100)
+			while(true)
 			{
+				while(true)//Repeats if the player got Ladder i.e option = 1
+				{
 				
-				diceValue = random.nextInt(6)+1;
-				option = random.nextInt(3);
-				count++;
+					diceValue = random.nextInt(6)+1;
+					option = random.nextInt(3);
+					firstPlayerChances++;
 			
+					if(option == 0) // if option = 0 No change in position 
+						break;
+					else if(option == 1)
+					{
+						firstPlayerPosition+=diceValue;
+						
+						if(firstPlayerPosition > 100)
+							firstPlayerPosition-=diceValue;
+					}
+					else if(option == 2)
+					{
+						firstPlayerPosition-=diceValue;
+						if(firstPlayerPosition < 0)
+							firstPlayerPosition=0;
+						break;
+					}		
+				}
+				while(true)//Repeats if the player got Ladder i.e option = 1
+				{
+				
+					diceValue = random.nextInt(6)+1;
+					option = random.nextInt(3);
+					secondPlayerChances++;
 			
-				if(option == 1) // if option = 0 No change in position 
-					position+=diceValue;
-			
-				else if(option == 2)
-					position-=diceValue;
-				
-				if(position < 0)
-					position=0;
-				
-				if(position > 100)
-					position-=diceValue;
-				
-				System.out.println("Position : "+position);
+					if(option == 0) // if option = 0 No change in position 
+						break;
+					else if(option == 1) 
+					{
+						secondPlayerPosition+=diceValue;
+						
+						if(secondPlayerPosition > 100)
+							secondPlayerPosition-=diceValue;
+					}
+					else if(option == 2)
+					{
+						secondPlayerPosition-=diceValue;
+						if(secondPlayerPosition < 0)
+							secondPlayerPosition=0;
+						break;
+					}
+				}
+				System.out.println("First Player Position : "+firstPlayerPosition+"\tSecond Player Position : "+secondPlayerPosition);
+
+				if(secondPlayerPosition == 100 || firstPlayerPosition == 100)
+					break;
 				
 			}
 			
-			System.out.println("No of times Dice rolled : "+count);
+			if(firstPlayerPosition == 100)
+			{
+				System.out.println("\n\t******  First Player Won the Game  ******");
+			}
+			else if(secondPlayerPosition == 100)
+				System.out.println("\n\t******  Second Player Won the Game  ******");
 			
-
+			System.out.println("\nNo of Chances got by First player : "+firstPlayerChances+"\n\nNo of Chances got by Second player : "+secondPlayerChances);
 	}
 
 }
