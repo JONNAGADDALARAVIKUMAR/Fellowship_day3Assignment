24a1bbe0e230c2ce12aa1ab93c5d55d567b84362
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-10-23 15:47:35 +0530

    UC 2

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,12 +1,20 @@
 package day2;
 
+import java.util.Random;
+
 public class SnakeLadder {
 
 	public static void main(String[] args) {
 		
-			System.out.println("Snake and Ladder game by Single player");
+			System.out.println("Snake and Ladder game by Single player\n\nThe game is supposed to start");
+			
+			
+			Random random = new Random();
+			int diceValue = random.nextInt(6)+1;
+			System.out.println("\nDice Value : "+diceValue);
+			
 			int position = 0;
-			System.out.println("\nThe game is supposed to start\n\nPOSITION : "+position);
+			System.out.println("\nPOSITION : "+position);
 		
 	}
 
