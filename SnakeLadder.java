14ae2ff857c53ab838a93cdb1e4ff60c39c844d3
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-10-23 15:41:06 +0530

    UC 1

---
+++ SnakeLadder.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		
+			System.out.println("Snake and Ladder game by Single player");
+			int position = 0;
+			System.out.println("\nThe game is supposed to start\n\nPOSITION : "+position);
+		
+	}
+
+}
